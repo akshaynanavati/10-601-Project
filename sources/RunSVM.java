5ab3397828c9444a26d19d2c846105bc883b30d1
YOUR-NAME <YOUR-EMAIL@DOMAIN.COM>
2014-12-10 16:57:57 -0500

    Bug fixes

--- sources/RunSVM.java
+++ sources/RunSVM.java
@@ -1,11 +1,16 @@
 import weka.classifiers.functions.LibSVM;
+import weka.core.Instances;
+
+import util.DataIO;
+import util.ClassifyInstances;
 
 public class RunSVM {
     private LibSVM model;
     private static final String help =
-        "Usage: java RunSVM <data-filename> <train-filename> <results-filename>"
+        "Usage: java RunSVM <data-filename> <train-filename> <results-filename>";
 
-    public RunSVM(String[] options, Instances data) {
+    public RunSVM(String[] options, Instances data)
+    throws Exception {
         this.model = new LibSVM();
         this.model.setOptions(options);
         data.setClassIndex(data.numAttributes() - 1);
@@ -14,7 +19,8 @@ public class RunSVM {
         System.out.println("Built classifier");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    throws Exception {
         if (args.length != 3) {
             System.out.println(RunSVM.help);
             return;
@@ -34,7 +40,7 @@ public class RunSVM {
         String[] results;
 
         System.out.println("Testing...");
-        results = ClassifyInstance.classify(model, test);
+        results = ClassifyInstances.classify(svm.model, test);
         DataIO.writeCSV(outputFname, results);
         System.out.println("Written results");
     }
